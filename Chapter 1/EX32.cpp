94503469d1437f8c4ab394416073d18512e685af
parissashahabi <prisashahbi@gmail.com>
2021-02-11 18:34:50 +0330

    Chapter 1 - Exercise 32

---
+++ Chapter 1/EX32.cpp
@@ -0,0 +1,18 @@
+// Exercise 1.32: ex_01_32.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n1, n2;
+
+    cout << "Three integer numbers: ";
+    cin >> n1 >> n2;
+
+    if ( n1 % n2 == 0 ) cout << n1 << " is a multiplication of " << n2 << endl;
+    else cout << n1 << " is NOT a multiplication of " << n2 << endl;
+
+    return 0;
+}
