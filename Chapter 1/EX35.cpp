ab5b5e21d7708654b945634c397795486a809d7e
parissashahabi <prisashahbi@gmail.com>
2021-02-11 18:54:47 +0330

    Chapter 1 - Exercise 35

---
+++ Chapter 1/EX35.cpp
@@ -0,0 +1,22 @@
+// Exercise 1.35: ex_01_35.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    char ch;
+
+    cout << "Character: ";
+    cin >> ch;
+
+    cout << "Integer equivalent is " << int( ch ) << endl;
+
+    return 0;
+}
+
+
+/**************************************************************************
+ *  https://en.wikipedia.org/wiki/ASCII                                   *
+ **************************************************************************/
