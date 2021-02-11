63155c63a17a5c4aef11180c5cc8cb1c665d2b22
parissashahabi <prisashahbi@gmail.com>
2021-02-11 17:55:59 +0330

    Chapter 1 - Exercise 24

---
+++ Chapter 1/EX24.cpp
@@ -0,0 +1,22 @@
+// Exercise 1.24: ex_01_24.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // a
+    cout << "1 2 3 4" << endl;
+
+    // b
+    cout << "1 " << "2 " << "3 " << "4\n";
+
+    // c
+    cout<<"1 ";
+    cout<<"2 ";
+    cout<<"3 ";
+    cout<<"4 "<<endl;
+
+    return 0;
+}
