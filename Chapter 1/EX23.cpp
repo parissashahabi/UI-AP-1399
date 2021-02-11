45200f3033f7347c1b54ad607f65a3a29fb9fe36
parissashahabi <prisashahbi@gmail.com>
2021-02-11 17:49:21 +0330

    Chapter 1 - Exercise 23

---
+++ Chapter 1/EX23.cpp
@@ -0,0 +1,22 @@
+// Exercise 1.23: ex_01_23.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int number1, number2;
+
+    cout << "Two numbers: ";
+    cin >> number1 >> number2;
+
+    cout << "\nSum is " << number1 + number2
+         << "\nProduct is " << number1 * number2
+         << "\nDifference is " << number1 - number2 << endl;
+
+    if ( number2 != 0 )
+        cout << "Quotient is " << number1 / number2 << endl;
+
+    return 0;
+}
