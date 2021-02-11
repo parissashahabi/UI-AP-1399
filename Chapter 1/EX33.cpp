386553feddbb5fe152fad2adee2fb4c0b211f513
parissashahabi <prisashahbi@gmail.com>
2021-02-11 18:36:37 +0330

    Chapter 1 - Exercise 33

---
+++ Chapter 1/EX33.cpp
@@ -0,0 +1,26 @@
+// Exercise 1.33: ex_01_33.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Eight statements
+    cout << "* * * * * * * *\n";
+    cout << " * * * * * * * *\n";
+    cout << "* * * * * * * *\n";
+    cout << " * * * * * * * *\n";
+    cout << "* * * * * * * *\n";
+    cout << " * * * * * * * *\n";
+    cout << "* * * * * * * *\n";
+    cout << " * * * * * * * *\n\n";
+
+    // One statement
+    cout << "* * * * * * * *\n * * * * * * * *\n"
+         << "* * * * * * * *\n * * * * * * * *\n"
+         << "* * * * * * * *\n * * * * * * * *\n"
+         << "* * * * * * * *\n * * * * * * * *\n" << endl;
+
+    return 0;
+}
