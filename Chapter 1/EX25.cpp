52c7bcd4d4d21d2d53b57382bfda94933b2adbb5
parissashahabi <prisashahbi@gmail.com>
2021-02-11 18:06:25 +0330

    Chapter 1 - Exercise 25

---
+++ Chapter 1/EX25.cpp
@@ -0,0 +1,25 @@
+// Exercise 1.25: ex_01_25.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int isLarger(int a, int b){
+    return a>b ? a : b;
+}
+
+int main() {
+
+    int number1, number2;
+
+    cout << "Two numbers: ";
+    cin >> number1 >> number2;
+
+    if ( number1 == number2 )
+        cout << "These numbers are equal.\n";
+
+    else
+        cout<<isLarger(number1,number2)<<" is larger."<<endl;
+
+    return 0;
+}
